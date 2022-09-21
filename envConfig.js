1a906bfa1be2c2f95e225b7ca6ec6133a4765dec
The-Orange-Dot <theorangedottwitch@gmail.com>
2022-09-22 01:42:14 +0900

    First init

---
+++ envConfig.js
@@ -0,0 +1,3 @@
+import { TWITCH_CLIENT_ID } from "@env";
+
+export default TWITCH_CLIENT_ID;
---
+++ types/env.d.ts
@@ -0,0 +1,5 @@
+declare module "@env" {
+  export const API_BASE: string;
+  export const TWITCH_CLIENT_ID: string;
+  export const TWITCH_CLIENT_SECRET: string;
+}
